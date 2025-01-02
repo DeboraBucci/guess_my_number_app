469dfc34029480a42a7ebcc5c143a996094032d2
Debora Bucci <dbucci@protonmail.com>
2025-01-02 20:30:24 -0300

    add some content, and create a function for picking a random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,39 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Title from "../components/ui/Title";
+import { useState } from "react";
 
-const GameScreen = () => {
-  return <Text>Game Screen!</Text>;
+function generateRandomNumber(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = ({ pickedNumber }) => {
+  const initialGuess = generateRandomNumber(1, 100, pickedNumber);
+
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      <View></View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+    paddingTop: StatusBar.currentHeight + 32,
+  },
+});
