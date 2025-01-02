5d9255669f31c626d5792ffb152e76e78dcd8a5c
Debora Bucci <dbucci@protonmail.com>
2025-01-02 16:01:41 -0300

    conditionally render either GameScreen or StartGameScreen depending on wether the user picked a number or not yet

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [pickedNumber, setPickedNumber] = useState();
+
+  const pickedNumberHandler = (pickedNum) => {
+    setPickedNumber(pickedNum);
+  };
+
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
       <ImageBackground
@@ -10,8 +18,12 @@ export default function App() {
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
+        onPickNumber={pickedNumberHandler}
       >
-        <StartGameScreen />
+        {!pickedNumber && (
+          <StartGameScreen onPickNumber={pickedNumberHandler} />
+        )}
+        {pickedNumber && <GameScreen />}
       </ImageBackground>
     </LinearGradient>
   );
