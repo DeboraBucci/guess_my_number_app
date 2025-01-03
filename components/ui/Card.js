8a9ffde94a92b4131979eb8125cadf1f697d2d3b
Debora Bucci <dbucci@protonmail.com>
2025-01-03 13:14:20 -0300

    create a card component

---
+++ components/ui/Card.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from "react-native";
+import Colors from "../../constants/colors";
+
+const Card = ({ children, title }) => {
+  return (
+    <View style={styles.card}>
+      {title && <Text style={styles.title}>{title}</Text>}
+      {children}
+    </View>
+  );
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 36,
+    borderRadius: 8,
+    backgroundColor: Colors.primary800,
+
+    // Shadow for Android
+    elevation: 4,
+
+    // Shadow for IOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  title: {
+    color: Colors.accent500,
+    fontSize: 24,
+  },
+});
